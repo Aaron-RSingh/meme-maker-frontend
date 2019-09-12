cb25bfec91d127e6ec6bf8e3ab548d7846d9d75c
Aaron <aaron.ramharacksingh@gmail.com>
2019-09-12 10:57:09 +0100

    routes now working

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,14 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 
 const Gallery = props => {
-  return (
-    <div className="row">
-      <div className="column">
-        {props.memes.map(meme => (
-          <img src={meme.name} />
-        ))}
-      </div>
-    </div>
-  );
+  debugger;
+  return <h1>This is where we will render allllll the pictures</h1>;
 };
+
 export default Gallery;
